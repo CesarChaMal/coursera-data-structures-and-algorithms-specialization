0361b7bec65cdd5a5beb2bbd0e31f9041ea59a78
Cesar Chavez <cesarchamal@gmail.com>
2025-03-25 03:05:09 +0100

    Update.

--- 01_toolbox/week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/FibonacciPartialSum.java
+++ 01_toolbox/week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/FibonacciPartialSum.java
@@ -19,12 +19,53 @@ public class FibonacciPartialSum {
 
         return sum % 10;
     }
-    
+
+    private static long getPisanoPeriod(long m) {
+        long prev = 0, curr = 1;
+        for (long i = 0; i < m * m; i++) {
+            long temp = (prev + curr) % m;
+            prev = curr;
+            curr = temp;
+
+            if (prev == 0 && curr == 1) {
+                return i + 1;
+            }
+        }
+        return 0;
+    }
+
+    private static long getFibonacciMod(long n, long m) {
+        long period = getPisanoPeriod(m);
+        n %= period;
+
+        long prev = 0, curr = 1;
+        if (n <= 1) return n;
+
+        for (long i = 2; i <= n; i++) {
+            long temp = (prev + curr) % m;
+            prev = curr;
+            curr = temp;
+        }
+
+        return curr;
+    }
+
+    private static long getFibonacciSum(long n) {
+        // sum of F(0) to F(n) = F(n+2) - 1
+        return (getFibonacciMod(n + 2, 10) + 9) % 10;
+    }
+
+    private static long getFibonacciPartialSumFast(long from, long to) {
+        long sumTo = getFibonacciSum(to);
+        long sumFromMinus1 = getFibonacciSum(from - 1);
+        return (sumTo - sumFromMinus1 + 10) % 10;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         long from = scanner.nextLong();
         long to = scanner.nextLong();
-        System.out.println(getFibonacciPartialSumNaive(from, to));
+        System.out.println(getFibonacciPartialSumFast(from, to));
     }
 }
 
--- 01_toolbox/week2_algorithmic_warmup/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/FibonacciSumSquares.java
+++ 01_toolbox/week2_algorithmic_warmup/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/FibonacciSumSquares.java
@@ -18,12 +18,47 @@ public class FibonacciSumSquares {
 
         return sum % 10;
     }
-    
+
+    private static long getPisanoPeriod(long m) {
+        long prev = 0, curr = 1;
+        for (long i = 0; i < m * m; i++) {
+            long temp = (prev + curr) % m;
+            prev = curr;
+            curr = temp;
+
+            if (prev == 0 && curr == 1)
+                return i + 1;
+        }
+        return 0;
+    }
+
+    private static long getFibonacciMod(long n, long m) {
+        long period = getPisanoPeriod(m);
+        n %= period;
+
+        long prev = 0, curr = 1;
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        for (long i = 2; i <= n; i++) {
+            long temp = (prev + curr) % m;
+            prev = curr;
+            curr = temp;
+        }
+
+        return curr;
+    }
+
+    private static long getFibonacciSumSquaresFast(long n) {
+        long Fn = getFibonacciMod(n, 10);
+        long FnPlus1 = getFibonacciMod(n + 1, 10);
+        return (Fn * FnPlus1) % 10;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         long n = scanner.nextLong();
-        long s = getFibonacciSumSquaresNaive(n);
-        System.out.println(s);
+        System.out.println(getFibonacciSumSquaresFast(n));
     }
 }
 
